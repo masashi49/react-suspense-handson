2ad65722f970f8c7c88c0b6f78c2579e1d50582c
yamadamasashi <shisama94@gmail.com>
2023-04-06 09:53:02 +0900

    試し中

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
-import './App.css'
+import "./App.css";
+import { Suspense } from "react";
+
+function sleep(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+const AlwaysSuspend: React.VFC = () => {
+  console.log("AlwaysSuspend is rendered");
+  throw sleep(1000);
+};
 
 function App() {
   return (
     <div className="text-center">
       <h1 className="text-2xl">React App!</h1>
+      <Suspense fallback={<p>Loading...</p>}>
+        <p>ここは表示される？</p>
+        <AlwaysSuspend />
+      </Suspense>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
